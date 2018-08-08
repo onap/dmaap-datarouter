e157c9c871ca96ef00fcb5fc591254ff3deb2778
eronkeo <ronan.keogh@ericsson.com>
2018-08-08 16:22:25 +0100

    remove tab characters from code
    Change-Id: I5b1d041561056833f744270350b3d2c9cd4e1dcb
    Issue-ID: DMAAP-570
    Signed-off-by: eronkeo <ronan.keogh@ericsson.com>

---
+++ datarouter-prov/src/test/java/datarouter/provisioning/IntegrationTestBase.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * ============LICENSE_START==================================================
+ * * org.onap.dmaap
+ * * ===========================================================================
+ * * Copyright © 2017 AT&T Intellectual Property. All rights reserved.
+ * * ===========================================================================
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * * you may not use this file except in compliance with the License.
+ * * You may obtain a copy of the License at
+ * *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ * *
+ *  * Unless required by applicable law or agreed to in writing, software
+ * * distributed under the License is distributed on an "AS IS" BASIS,
+ * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * * See the License for the specific language governing permissions and
+ * * limitations under the License.
+ * * ============LICENSE_END====================================================
+ * *
+ * * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * *
+ ******************************************************************************/
+
+package datarouter.provisioning;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.util.Properties;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.junit.After;
+import org.junit.Before;
+import org.onap.dmaap.datarouter.provisioning.FeedServlet;
+
+public class IntegrationTestBase {
+    /** The properties file to read the DB properties from. */
+    public static final String CONFIG_FILE = "tests.properties";
+
+    public Properties props;
+    protected AbstractHttpClient httpclient;
+    protected String s33;
+    protected String s257;
+    protected static JSONObject db_state;
+
+    /**
+     * This is the setUp method.
+     */
+    @Before
+    public void setUp() throws Exception {
+        if (props == null) {
+            props = new Properties();
+            InputStream inStream = getClass().getClassLoader().getResourceAsStream(CONFIG_FILE);
+            try {
+                props.load(inStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                inStream.close();
+            }
+        }
+
+        httpclient = new DefaultHttpClient();
+        String str = "0123456789ABCDEF";
+        s33 = str + str + "!";
+        str = str + str + str + str;
+        s257 = str + str + str + str + "!";
+
+        // keystore
+        String store = props.getProperty("test.keystore");
+        String pass  = props.getProperty("test.kspassword");
+        KeyStore keyStore  = KeyStore.getInstance(KeyStore.getDefaultType());
+        FileInputStream instream = new FileInputStream(new File(store));
+        try {
+            keyStore.load(instream, pass.toCharArray());
+        } catch (Exception x) {
+            System.err.println("READING KEYSTORE: " + x);
+        } finally {
+            try {
+                instream.close();
+            } catch (Exception ignore) {
+                // Ignore exception
+            }
+        }
+
+        store = props.getProperty("test.truststore");
+        pass  = props.getProperty("test.tspassword");
+        KeyStore trustStore  = KeyStore.getInstance(KeyStore.getDefaultType());
+        instream = new FileInputStream(new File(store));
+        try {
+            trustStore.load(instream, pass.toCharArray());
+        } catch (Exception x) {
+            System.err.println("READING TRUSTSTORE: " + x);
+        } finally {
+            try {
+                instream.close();
+            } catch (Exception ignore) {
+                // Ignore exception
+            }
+        }
+
+        SSLSocketFactory socketFactory = new SSLSocketFactory(keyStore, "changeit", trustStore);
+        Scheme sch = new Scheme("https", 443, socketFactory);
+        httpclient.getConnectionManager().getSchemeRegistry().register(sch);
+    }
+
+    /**
+     * This is the getDBstate method.
+     */
+    public JSONObject getDBstate() {
+        // set db_state!
+        if (db_state == null) {
+            String url   = props.getProperty("test.host") + "/internal/prov";
+            HttpGet httpGet = new HttpGet(url);
+            try {
+                httpGet.addHeader(FeedServlet.BEHALF_HEADER, "JUnit");
+                HttpResponse response = httpclient.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+                String ctype = entity.getContentType().getValue().trim();
+                // save the response body as db_state
+                boolean ok  = ctype.equals(FeedServlet.PROVFULL_CONTENT_TYPE1);
+                ok |= ctype.equals(FeedServlet.PROVFULL_CONTENT_TYPE2);
+                if (ok) {
+                    db_state = null;
+                    try {
+                        db_state = new JSONObject(new JSONTokener(entity.getContent()));
+                    } catch (Exception e) {
+                        fail("Bad JSON: " + e.getMessage());
+                    }
+                } else {
+                    EntityUtils.consume(entity);
+                }
+            } catch (IOException e) {
+                fail(e.getMessage());
+            } finally {
+                httpGet.releaseConnection();
+            }
+        }
+        return db_state;
+    }
+
+    /**
+     * This is the tearDown method.
+     */
+    @After
+    public void tearDown() throws Exception {
+        // When HttpClient instance is no longer needed,
+        // shut down the connection manager to ensure
+        // immediate deallocation of all system resources
+        httpclient.getConnectionManager().shutdown();
+    }
+
+    protected void ckResponse(HttpResponse response, int expect) {
+        System.out.println(response.getStatusLine());
+        StatusLine sl = response.getStatusLine();
+        int code = sl.getStatusCode();
+        if (code != expect) {
+            fail("Unexpected response, expect " + expect + " got " + code + " " + sl.getReasonPhrase());
+        }
+    }
+}
---
+++ datarouter-prov/src/test/java/datarouter/provisioning/IntegrationTestLogGet.java
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * ============LICENSE_START==================================================
+ * * org.onap.dmaap
+ * * ===========================================================================
+ * * Copyright © 2017 AT&T Intellectual Property. All rights reserved.
+ * * ===========================================================================
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * * you may not use this file except in compliance with the License.
+ * * You may obtain a copy of the License at
+ * *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ * *
+ *  * Unless required by applicable law or agreed to in writing, software
+ * * distributed under the License is distributed on an "AS IS" BASIS,
+ * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * * See the License for the specific language governing permissions and
+ * * limitations under the License.
+ * * ============LICENSE_END====================================================
+ * *
+ * * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * *
+ ******************************************************************************/
+
+package datarouter.provisioning;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONTokener;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.onap.dmaap.datarouter.provisioning.FeedServlet;
+
+public class IntegrationTestLogGet extends IntegrationTestBase {
+    private JSONArray returnedlist;
+    private int feedid = 4;
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // need to seed the DB here
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        // need to "unseed" the DB
+    }
+
+    /**
+     * This is the setUp method.
+     */
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        getDBstate();
+//        JSONArray ja = db_state.getJSONArray("feeds");
+//        for (int i = 0; i < ja.length(); i++) {
+//            JSONObject jo = ja.getJSONObject(i);
+//            if (!jo.getBoolean("deleted"))
+//                feedid = jo.getInt("feedid");
+//        }
+    }
+
+    @Test
+    public void testNormal() {
+        testCommon(HttpServletResponse.SC_OK);
+    }
+
+    @Test
+    public void testNormalPubOnly() {
+        testCommon(HttpServletResponse.SC_OK, "?type=pub");
+    }
+
+    @Test
+    public void testNormalDelOnly() {
+        testCommon(HttpServletResponse.SC_OK, "?type=del");
+    }
+
+    @Test
+    public void testNormalExpOnly() {
+        testCommon(HttpServletResponse.SC_OK, "?type=exp");
+    }
+
+    @Test
+    public void testNormalXxxOnly() {
+        testCommon(HttpServletResponse.SC_BAD_REQUEST, "?type=xxx");
+    }
+
+    @Test
+    public void testNormalStatusSuccess() {
+        testCommon(HttpServletResponse.SC_OK, "?statusCode=success");
+    }
+
+    @Test
+    public void testNormalStatusRedirect() {
+        testCommon(HttpServletResponse.SC_OK, "?statusCode=redirect");
+    }
+
+    @Test
+    public void testNormalStatusFailure() {
+        testCommon(HttpServletResponse.SC_OK, "?statusCode=failure");
+    }
+
+    @Test
+    public void testNormalStatus200() {
+        testCommon(HttpServletResponse.SC_OK, "?statusCode=200");
+    }
+
+    @Test
+    public void testNormalStatusXxx() {
+        testCommon(HttpServletResponse.SC_BAD_REQUEST, "?statusCode=xxx");
+    }
+
+    @Test
+    public void testNormalExpiryNotRetryable() {
+        testCommon(HttpServletResponse.SC_OK, "?expiryReason=notRetryable");
+    }
+
+    @Test
+    public void testNormalExpiryRetriesExhausted() {
+        testCommon(HttpServletResponse.SC_OK, "?expiryReason=retriesExhausted");
+    }
+
+    @Test
+    public void testNormalExpiryXxx() {
+        testCommon(HttpServletResponse.SC_BAD_REQUEST, "?expiryReason=xxx");
+    }
+
+    @Test
+    public void testNormalPublishId() {
+        testCommon(HttpServletResponse.SC_OK, "?publishId=1366985877801.mtdvnj00-drtr.proto.research.att.com");
+    }
+
+    @Test
+    public void testNormalStart() {
+        long nowMinus5Days = System.currentTimeMillis() - (5 * 24 * 60 * 60 * 1000L);    // 5 days
+        testCommon(HttpServletResponse.SC_OK, String.format("?start=%s", sdf.format(nowMinus5Days)));
+    }
+
+    @Test
+    public void testBadStart() {
+        testCommon(HttpServletResponse.SC_BAD_REQUEST, "?start=xxx");
+    }
+
+    @Test
+    public void testLongEnd() {
+        testCommon(HttpServletResponse.SC_OK, "?end=1364837896220");
+    }
+
+    @Test
+    public void testBadEnd() {
+        testCommon(HttpServletResponse.SC_BAD_REQUEST, "?end=2013-04-25T11:01:25Q");
+    }
+
+    private void testCommon(int expect) {
+        testCommon(expect, "");
+    }
+
+    private void testCommon(int expect, String query) {
+        String url = props.getProperty("test.host") + "/feedlog/" + feedid + query;
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = httpclient.execute(httpGet);
+            ckResponse(response, expect);
+
+            HttpEntity entity = response.getEntity();
+            String ctype = entity.getContentType().getValue().trim();
+            if (expect == HttpServletResponse.SC_OK) {
+                if (!ctype.equals(FeedServlet.LOGLIST_CONTENT_TYPE)) {
+                    fail("Got wrong content type: " + ctype);
+                }
+            }
+
+            // do something useful with the response body and ensure it is fully consumed
+            if (ctype.equals(FeedServlet.LOGLIST_CONTENT_TYPE)) {
+                try {
+                    returnedlist = new JSONArray(new JSONTokener(entity.getContent()));
+                    int returnedListLength = returnedlist.length();
+                    if (returnedListLength != 0) {
+                        System.err.println(returnedListLength + " items");
+                    }
+                } catch (Exception e) {
+                    fail("Bad JSON: " + e.getMessage());
+                }
+            } else {
+                EntityUtils.consume(entity);
+            }
+        } catch (IOException e) {
+            fail(e.getMessage());
+        } finally {
+            httpGet.releaseConnection();
+        }
+    }
+}
