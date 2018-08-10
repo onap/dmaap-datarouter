50dddf6a9ddd7dbe5c434b2849feaee67f347ea3
eronkeo <ronan.keogh@ericsson.com>
2018-08-10 15:43:37 +0100

    datarouter-node code clean - remove tabs
    Change-Id: I352b9e380bd6f74e5e5421265ace0a8e89e1435f
    Issue-ID: DMAAP-582
    Signed-off-by: eronkeo <ronan.keogh@ericsson.com>

--- datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/DeliveryQueueHelper.java
+++ datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/DeliveryQueueHelper.java
@@ -7,9 +7,9 @@
  * * Licensed under the Apache License, Version 2.0 (the "License");
  * * you may not use this file except in compliance with the License.
  * * You may obtain a copy of the License at
- * * 
+ * *
  *  *      http://www.apache.org/licenses/LICENSE-2.0
- * * 
+ * *
  *  * Unless required by applicable law or agreed to in writing, software
  * * distributed under the License is distributed on an "AS IS" BASIS,
  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,65 +25,79 @@
 package org.onap.dmaap.datarouter.node;
 
 /**
- *	Interface to allow independent testing of the DeliveryQueue code
- *	<p>
- *	This interface represents all of the configuration information and
- *	feedback mechanisms that a delivery queue needs.
+ * Interface to allow independent testing of the DeliveryQueue code
+ * <p>
+ * This interface represents all of the configuration information and
+ * feedback mechanisms that a delivery queue needs.
  */
-public interface	DeliveryQueueHelper	{
-	/**
-	 *	Get the timeout (milliseconds) before retrying after an initial delivery failure
-	 */
-	public long getInitFailureTimer();
-	/**
-	 *	Get the ratio between timeouts on consecutive delivery attempts
-	 */
-	public double	getFailureBackoff();
-	/**
-	 *	Get the maximum timeout (milliseconds) between delivery attempts
-	 */
-	public long	getMaxFailureTimer();
-	/**
-	 *	Get the expiration timer (milliseconds) for deliveries
-	 */
-	public long	getExpirationTimer();
-	/**
-	 *	Get the maximum number of file delivery attempts before checking
-	 *	if another queue has work to be performed.
-	 */
-	public int getFairFileLimit();
-	/**
-	 *	Get the maximum amount of time spent delivering files before checking if another queue has work to be performed.
-	 */
-	public long getFairTimeLimit();
-	/**
-	 *	Get the URL for delivering a file
-	 *	@param dest	The destination information for the file to be delivered.
-	 *	@param fileid	The file id for the file to be delivered.
-	 *	@return	The URL for delivering the file (typically, dest.getURL() + "/" + fileid).
-	 */
-	public String	getDestURL(DestInfo dest, String fileid);
-	/**
-	 *	Forget redirections associated with a subscriber
-	 *	@param	dest	Destination information to forget
-	 */
-	public void	handleUnreachable(DestInfo dest);
-	/**
-	 *	Post redirection for a subscriber
-	 *	@param	dest	Destination information to update
-	 *	@param	location	Location given by subscriber
-	 *	@param	fileid	File ID of request
-	 *	@return	true if this 3xx response is retryable, otherwise, false.
-	 */
-	public boolean	handleRedirection(DestInfo dest, String location, String fileid);
-	/**
-	 *	Should I handle 3xx responses differently than 4xx responses?
-	 */
-	public boolean	isFollowRedirects();
-	/**
-	 *	Get the feed ID for a subscription
-	 *	@param subid	The subscription ID
-	 *	@return	The feed ID
-	 */
-	public String getFeedId(String subid);
+public interface DeliveryQueueHelper {
+    /**
+     * Get the timeout (milliseconds) before retrying after an initial delivery failure
+     */
+    public long getInitFailureTimer();
+
+    /**
+     * Get the ratio between timeouts on consecutive delivery attempts
+     */
+    public double getFailureBackoff();
+
+    /**
+     * Get the maximum timeout (milliseconds) between delivery attempts
+     */
+    public long getMaxFailureTimer();
+
+    /**
+     * Get the expiration timer (milliseconds) for deliveries
+     */
+    public long getExpirationTimer();
+
+    /**
+     * Get the maximum number of file delivery attempts before checking
+     * if another queue has work to be performed.
+     */
+    public int getFairFileLimit();
+
+    /**
+     * Get the maximum amount of time spent delivering files before checking if another queue has work to be performed.
+     */
+    public long getFairTimeLimit();
+
+    /**
+     * Get the URL for delivering a file
+     *
+     * @param dest   The destination information for the file to be delivered.
+     * @param fileid The file id for the file to be delivered.
+     * @return The URL for delivering the file (typically, dest.getURL() + "/" + fileid).
+     */
+    public String getDestURL(DestInfo dest, String fileid);
+
+    /**
+     * Forget redirections associated with a subscriber
+     *
+     * @param    dest    Destination information to forget
+     */
+    public void handleUnreachable(DestInfo dest);
+
+    /**
+     * Post redirection for a subscriber
+     *
+     * @param    dest    Destination information to update
+     * @param    location    Location given by subscriber
+     * @param    fileid    File ID of request
+     * @return true if this 3xx response is retryable, otherwise, false.
+     */
+    public boolean handleRedirection(DestInfo dest, String location, String fileid);
+
+    /**
+     * Should I handle 3xx responses differently than 4xx responses?
+     */
+    public boolean isFollowRedirects();
+
+    /**
+     * Get the feed ID for a subscription
+     *
+     * @param subid The subscription ID
+     * @return The feed ID
+     */
+    public String getFeedId(String subid);
 }
