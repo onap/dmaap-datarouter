1841cb5d8da7b21996f8faad9d24f858e6ce8a41
eronkeo <ronan.keogh@ericsson.com>
2018-08-12 15:46:43 +0100

    datarouter-node clean code - remove tabs
    Change-Id: I28240cee3958603788d1e9f8e72bb10706dba712
    Issue-ID: DMAAP-582
    Signed-off-by: eronkeo <ronan.keogh@ericsson.com>

--- datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/NodeMain.java
+++ datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/NodeMain.java
@@ -7,9 +7,9 @@
  * * Licensed under the Apache License, Version 2.0 (the "License");
  * * you may not use this file except in compliance with the License.
  * * You may obtain a copy of the License at
- * * 
+ * *
  *  *      http://www.apache.org/licenses/LICENSE-2.0
- * * 
+ * *
  *  * Unless required by applicable law or agreed to in writing, software
  * * distributed under the License is distributed on an "AS IS" BASIS,
  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,82 +32,91 @@ import org.eclipse.jetty.server.ssl.*;
 import org.apache.log4j.Logger;
 
 /**
- *	The main starting point for the Data Router node
+ * The main starting point for the Data Router node
  */
-public class NodeMain	{
-	private NodeMain() {}
-	private static Logger	logger = Logger.getLogger("org.onap.dmaap.datarouter.node.NodeMain");
-	private static class wfconfig implements Runnable	{
-		private NodeConfigManager ncm;
-		public wfconfig(NodeConfigManager ncm) {
-			this.ncm = ncm;
-		}
-		public synchronized void run() {
-			notify();
-		}
-		public synchronized void waitforconfig() {
-			ncm.registerConfigTask(this);
-			while (!ncm.isConfigured()) {
-				logger.info("NODE0003 Waiting for Node Configuration");
-				try {
-					wait();
-				} catch (Exception e) {
-				}
-			}
-			ncm.deregisterConfigTask(this);
-			logger.info("NODE0004 Node Configuration Data Received");
-		}
-	}
-	private static Delivery d;
-	private static NodeConfigManager ncm;
-	/**
-	 *	Reset the retry timer for a subscription
-	 */
-	public static void resetQueue(String subid, String ip) {
-		d.resetQueue(ncm.getSpoolDir(subid, ip));
-	}
-	/**
-	 *	Start the data router.
-	 *	<p>
-	 *	The location of the node configuration file can be set using the
-	 *	org.onap.dmaap.datarouter.node.ConfigFile system property.  By
-	 *	default, it is "etc/node.properties".
-	 */
-	public static void main(String[] args) throws Exception {
-		logger.info("NODE0001 Data Router Node Starting");
-		IsFrom.setDNSCache();
-		ncm = NodeConfigManager.getInstance();
-		logger.info("NODE0002 I am " + ncm.getMyName());
-		(new wfconfig(ncm)).waitforconfig();
-		d = new Delivery(ncm);
-		LogManager lm = new LogManager(ncm);
-		Server server = new Server();
-		SelectChannelConnector http = new SelectChannelConnector();
-		http.setPort(ncm.getHttpPort());
-		http.setMaxIdleTime(2000);
-		http.setRequestHeaderSize(2048);
-		SslSelectChannelConnector https = new SslSelectChannelConnector();
-		https.setPort(ncm.getHttpsPort());
-		https.setMaxIdleTime(30000);
-		https.setRequestHeaderSize(8192);
-		SslContextFactory cf = https.getSslContextFactory();
-		
-		/**Skip SSLv3 Fixes*/
-		cf.addExcludeProtocols("SSLv3");
-		logger.info("Excluded protocols node-"+cf.getExcludeProtocols());
-		/**End of SSLv3 Fixes*/
+public class NodeMain {
+    private NodeMain() {
+    }
 
-		cf.setKeyStoreType(ncm.getKSType());
-		cf.setKeyStorePath(ncm.getKSFile());
-		cf.setKeyStorePassword(ncm.getKSPass());
-		cf.setKeyManagerPassword(ncm.getKPass());
-		server.setConnectors(new Connector[] { http, https });
-		ServletContextHandler ctxt = new ServletContextHandler(0);
-		ctxt.setContextPath("/");
-		server.setHandler(ctxt);
-		ctxt.addServlet(new ServletHolder(new NodeServlet()), "/*");
-		logger.info("NODE0005 Data Router Node Activating Service");
-		server.start();
-		server.join();
-	}
+    private static Logger logger = Logger.getLogger("org.onap.dmaap.datarouter.node.NodeMain");
+
+    private static class wfconfig implements Runnable {
+        private NodeConfigManager ncm;
+
+        public wfconfig(NodeConfigManager ncm) {
+            this.ncm = ncm;
+        }
+
+        public synchronized void run() {
+            notify();
+        }
+
+        public synchronized void waitforconfig() {
+            ncm.registerConfigTask(this);
+            while (!ncm.isConfigured()) {
+                logger.info("NODE0003 Waiting for Node Configuration");
+                try {
+                    wait();
+                } catch (Exception e) {
+                }
+            }
+            ncm.deregisterConfigTask(this);
+            logger.info("NODE0004 Node Configuration Data Received");
+        }
+    }
+
+    private static Delivery d;
+    private static NodeConfigManager ncm;
+
+    /**
+     * Reset the retry timer for a subscription
+     */
+    public static void resetQueue(String subid, String ip) {
+        d.resetQueue(ncm.getSpoolDir(subid, ip));
+    }
+
+    /**
+     * Start the data router.
+     * <p>
+     * The location of the node configuration file can be set using the
+     * org.onap.dmaap.datarouter.node.ConfigFile system property.  By
+     * default, it is "etc/node.properties".
+     */
+    public static void main(String[] args) throws Exception {
+        logger.info("NODE0001 Data Router Node Starting");
+        IsFrom.setDNSCache();
+        ncm = NodeConfigManager.getInstance();
+        logger.info("NODE0002 I am " + ncm.getMyName());
+        (new wfconfig(ncm)).waitforconfig();
+        d = new Delivery(ncm);
+        LogManager lm = new LogManager(ncm);
+        Server server = new Server();
+        SelectChannelConnector http = new SelectChannelConnector();
+        http.setPort(ncm.getHttpPort());
+        http.setMaxIdleTime(2000);
+        http.setRequestHeaderSize(2048);
+        SslSelectChannelConnector https = new SslSelectChannelConnector();
+        https.setPort(ncm.getHttpsPort());
+        https.setMaxIdleTime(30000);
+        https.setRequestHeaderSize(8192);
+        SslContextFactory cf = https.getSslContextFactory();
+
+        /**Skip SSLv3 Fixes*/
+        cf.addExcludeProtocols("SSLv3");
+        logger.info("Excluded protocols node-" + cf.getExcludeProtocols());
+        /**End of SSLv3 Fixes*/
+
+        cf.setKeyStoreType(ncm.getKSType());
+        cf.setKeyStorePath(ncm.getKSFile());
+        cf.setKeyStorePassword(ncm.getKSPass());
+        cf.setKeyManagerPassword(ncm.getKPass());
+        server.setConnectors(new Connector[]{http, https});
+        ServletContextHandler ctxt = new ServletContextHandler(0);
+        ctxt.setContextPath("/");
+        server.setHandler(ctxt);
+        ctxt.addServlet(new ServletHolder(new NodeServlet()), "/*");
+        logger.info("NODE0005 Data Router Node Activating Service");
+        server.start();
+        server.join();
+    }
 }
--- datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/eelf/EELFFilter.java
+++ datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/eelf/EELFFilter.java
@@ -7,9 +7,9 @@
  * * Licensed under the Apache License, Version 2.0 (the "License");
  * * you may not use this file except in compliance with the License.
  * * You may obtain a copy of the License at
- * * 
+ * *
  *  *      http://www.apache.org/licenses/LICENSE-2.0
- * * 
+ * *
  *  * Unless required by applicable law or agreed to in writing, software
  * * distributed under the License is distributed on an "AS IS" BASIS,
  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,15 +29,15 @@ import ch.qos.logback.core.spi.FilterReply;
 /*
  * When EELF functionality added it default started logging Jetty logs as well which in turn stopped existing functionality of logging jetty statements in node.log
  * added code in logback.xml to add jetty statements in node.log.
- * This class removes extran EELF statements from node.log since they are being logged in apicalls.log 
+ * This class removes extran EELF statements from node.log since they are being logged in apicalls.log
  */
-public class EELFFilter extends Filter<ILoggingEvent>{
-	  @Override
-	  public FilterReply decide(ILoggingEvent event) {    
-	    if (event.getMessage().contains("EELF")) {
-	      return FilterReply.DENY;
-	    } else {
-	      return FilterReply.ACCEPT;
-	    }
-	  }
+public class EELFFilter extends Filter<ILoggingEvent> {
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+        if (event.getMessage().contains("EELF")) {
+            return FilterReply.DENY;
+        } else {
+            return FilterReply.ACCEPT;
+        }
+    }
 }
