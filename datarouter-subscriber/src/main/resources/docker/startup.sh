aa988a2dd377cbd8e675b7f95636a4e4a3cef17a
Fiachra Corcoran <fiachra.corcoran@ericsson.com>
2018-08-30 00:43:55 +0100

    Additional updates for Subscriber docker image
    Issue-ID: DMAAP-662
    Change-Id: I4d850024cc0df238d7d44d74bed1b99169b1374c
    Signed-off-by: Fiachra Corcoran <fiachra.corcoran@ericsson.com>

--- datarouter-subscriber/src/main/resources/docker/startup.sh
+++ datarouter-subscriber/src/main/resources/docker/startup.sh
@@ -5,7 +5,7 @@ CLASSPATH=$ETC
 for FILE in `find $LIB -name *.jar`; do
   CLASSPATH=$CLASSPATH:$FILE
 done
-java -classpath $CLASSPATH  org.onap.dmaap.datarouter.subscriber.Subscriber
+java -classpath $CLASSPATH  org.onap.dmaap.datarouter.subscriber.SubscriberMain
 
 runner_file="$LIB/subscriber-jar-with-dependencies.jar"
 echo "Starting using" $runner_file
