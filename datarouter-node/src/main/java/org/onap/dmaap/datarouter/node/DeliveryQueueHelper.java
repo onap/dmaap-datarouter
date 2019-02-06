85d71202a672be9f91443e7c4c1b816371d42821
econwar <conor.ward@est.tech>
2019-02-06 14:43:19 +0000

    Add optional API for PM Mapper
    Added new field to Subscriber class to keep files after published
    Added new Delete endpoint so that file can then be deleted
    
    Change-Id: Id72da67689a7ceda8ddd4997cd6349b981cb1cdb
    Issue-ID: DMAAP-981
    Signed-off-by: econwar <conor.ward@est.tech>

--- datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/DeliveryQueueHelper.java
+++ datarouter-node/src/main/java/org/onap/dmaap/datarouter/node/DeliveryQueueHelper.java
@@ -34,64 +34,69 @@ public interface DeliveryQueueHelper {
     /**
      * Get the timeout (milliseconds) before retrying after an initial delivery failure
      */
-    public long getInitFailureTimer();
+    long getInitFailureTimer();
+
+    /**
+     * Get the timeout before retrying after delivery and wait for file processing
+     */
+    long getWaitForFileProcessFailureTimer();
 
     /**
      * Get the ratio between timeouts on consecutive delivery attempts
      */
-    public double getFailureBackoff();
+    double getFailureBackoff();
 
     /**
      * Get the maximum timeout (milliseconds) between delivery attempts
      */
-    public long getMaxFailureTimer();
+    long getMaxFailureTimer();
 
     /**
      * Get the expiration timer (milliseconds) for deliveries
      */
-    public long getExpirationTimer();
+    long getExpirationTimer();
 
     /**
      * Get the maximum number of file delivery attempts before checking
      * if another queue has work to be performed.
      */
-    public int getFairFileLimit();
+    int getFairFileLimit();
 
     /**
      * Get the maximum amount of time spent delivering files before checking if another queue has work to be performed.
      */
-    public long getFairTimeLimit();
+    long getFairTimeLimit();
 
     /**
      * Get the URL for delivering a file
      *
-     * @param dest   The destination information for the file to be delivered.
+     * @param destinationInfo   The destination information for the file to be delivered.
      * @param fileid The file id for the file to be delivered.
-     * @return The URL for delivering the file (typically, dest.getURL() + "/" + fileid).
+     * @return The URL for delivering the file (typically, destinationInfo.getURL() + "/" + fileid).
      */
-    public String getDestURL(DestInfo dest, String fileid);
+    String getDestURL(DestInfo destinationInfo, String fileid);
 
     /**
      * Forget redirections associated with a subscriber
      *
-     * @param    dest    Destination information to forget
+     * @param    destinationInfo    Destination information to forget
      */
-    public void handleUnreachable(DestInfo dest);
+    void handleUnreachable(DestInfo destinationInfo);
 
     /**
      * Post redirection for a subscriber
      *
-     * @param    dest    Destination information to update
+     * @param    destinationInfo    Destination information to update
      * @param    location    Location given by subscriber
      * @param    fileid    File ID of request
      * @return true if this 3xx response is retryable, otherwise, false.
      */
-    public boolean handleRedirection(DestInfo dest, String location, String fileid);
+    boolean handleRedirection(DestInfo destinationInfo, String location, String fileid);
 
     /**
      * Should I handle 3xx responses differently than 4xx responses?
      */
-    public boolean isFollowRedirects();
+    boolean isFollowRedirects();
 
     /**
      * Get the feed ID for a subscription
@@ -99,5 +104,5 @@ public interface DeliveryQueueHelper {
      * @param subid The subscription ID
      * @return The feed ID
      */
-    public String getFeedId(String subid);
+    String getFeedId(String subid);
 }
