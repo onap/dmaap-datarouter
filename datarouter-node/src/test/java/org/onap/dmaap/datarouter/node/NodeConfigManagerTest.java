4f94c02e3c1b33cbc9dae0d00d03321cef943100
efiacor <fiachra.corcoran@est.tech>
2019-08-29 12:08:28 +0000

    Make stylecheck mandatory
    Change-Id: I90003b23994ee6f294c7079845d57c6467655e7e
    Issue-ID: DMAAP-1250
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>

--- datarouter-node/src/test/java/org/onap/dmaap/datarouter/node/NodeConfigManagerTest.java
+++ datarouter-node/src/test/java/org/onap/dmaap/datarouter/node/NodeConfigManagerTest.java
@@ -24,6 +24,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URL;
+
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.Assert;
